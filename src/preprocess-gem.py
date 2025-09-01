359f163803e68267a28a92d999b875f0464c453b
Kowshika Sarker <63386569+kowshikasarker@users.noreply.github.com>
2025-09-01 17:55:22 -0500

    Update and rename preprocess-human-gem.py to preprocess-gem.py

--- src/preprocess-gem.py
+++ src/preprocess-gem.py
@@ -1,76 +1,90 @@
 import argparse
+import sys
 import pandas as pd
 from pathlib import Path
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-g", "--gem_path", type=str,
-                        help="path to end metabolomics profile in xlsx format",
+    parser = argparse.ArgumentParser(description='Discards and prerpocesses GEM reactions based on reaction reversibility and metabolite overlap filters.')
+    
+    parser.add_argument("--gem_path", type=str,
+                        help="path to a gem model in .xlsx format",
                         required=True, default=None)
-    parser.add_argument("-m", "--met_75_path", type=str,
-                        help="path to list of 75 metabolites in tsv format", required=True, default=None)
-    parser.add_argument("-o", "--out_dir", type=str,
+    parser.add_argument("--valid_met_path", type=str,
+                        help="path to list of valid (common between user data and gem) metabolites in tsv format",
+                        required=True, default=None)
+    
+    parser.add_argument("--log_path", type=str,
+                        help="path to log file",
+                        required=True, default=None)
+    parser.add_argument("--out_dir", type=str,
                         help="path to output dir",
                         required=True, default=None)
     args = parser.parse_args()
     return args
 
 def extract_metabolites(eqn):
+    print('eqn', eqn)
     old = eqn.split(' + ')
+    print('old', old)
     new = set()
     flag = False
     for i_old in old:
+        print(i_old)
         i_old_split = i_old.split(' ')
+        print(i_old)
         if(len(i_old_split)>1):
-            if(i_old_split[0].replace(".", "").isnumeric()): # If a metabolite has a coefficient in reaction equation
+            print('if')
+            if(i_old_split[0].replace(".", "").isnumeric()): # If a metabolite   a coefficient in reaction equation
                 i_new = i_old[len(i_old_split[0])+1:]
                 print(i_new, i_old)
-                #i_new = i_old.replace(i_old_split[0] + ' ', '')
-                #print(i_old, '->', i_new)
+                
                 new.add(i_new)
                 flag = True
             else:
                 new.add(i_old)
         else:
+            
             new.add(i_old)
-    '''if(flag):
-        print('old', old)
-        print('new', new)
-        print()'''
+    print('new', new)
     return new
             
 
-def all_reactions(met_75_path, gem_path, out_dir):
-    met75 = pd.read_csv(met_75_path, sep='\t')
-    met75 = set(met75['MET_ID'])
+def all_reactions(valid_met_path, gem_path, out_dir):
+    valid_mets = pd.read_csv(valid_met_path, sep='\t')
+    valid_mets = set(valid_mets['MET_ID'])
     react_gem = pd.read_excel(gem_path, sheet_name='RXNS', usecols=['ID', 'EQUATION', 'SUBSYSTEM'])
-
     react_gem = react_gem.rename(columns={'ID': 'RXN_ID'})
+    
+    print(react_gem.head())
     pattern = '|'.join(['\[e\]', '\[x\]', '\[m\]', '\[c\]', '\[l\]', '\[r\]', '\[g\]', '\[n\]', '\[i\]'])
-    react_gem['EQUATION'] = react_gem['EQUATION'].str.replace(pattern, '')
+    react_gem['EQUATION'] = react_gem['EQUATION'].str.replace(pattern, '', regex=True)
+    print(react_gem.head())
     react_gem['Direction'] = react_gem['EQUATION'].apply(lambda x: 2 if '<=>' in x else 1)
     react_gem['EQUATION'] = react_gem['EQUATION'].str.replace('<=>', '=>')
     react_gem[['EQUATION_LHS', 'EQUATION_RHS']] = react_gem['EQUATION'].str.split(' => ', expand=True)
     react_gem['Substrate_Set'] = react_gem['EQUATION_LHS'].apply(lambda x: extract_metabolites(x))
     react_gem['Product_Set'] = react_gem['EQUATION_RHS'].apply(lambda x: extract_metabolites(x))
     react_gem['Metabolite_Set'] = react_gem.apply(lambda x: x['Product_Set'].union(x['Substrate_Set']), axis=1)
+    
+    print(react_gem)
 
     react_gem['Substrate_Count'] = react_gem['Substrate_Set'].apply(lambda x: len(x))
     react_gem['Product_Count'] = react_gem['Product_Set'].apply(lambda x: len(x))
     react_gem['Metabolite_Count'] = react_gem['Metabolite_Set'].apply(lambda x: len(x))
-    react_gem['Measured_Substrate'] = react_gem['Substrate_Set'].apply(lambda x: x.intersection(met75))
-    react_gem['Measured_Product'] = react_gem['Product_Set'].apply(lambda x: x.intersection(met75))
-    react_gem['Measured_Metabolite'] = react_gem['Metabolite_Set'].apply(lambda x: x.intersection(met75))
+    
+    react_gem['Measured_Substrate'] = react_gem['Substrate_Set'].apply(lambda x: x.intersection(valid_mets))
+    react_gem['Measured_Product'] = react_gem['Product_Set'].apply(lambda x: x.intersection(valid_mets))
+    react_gem['Measured_Metabolite'] = react_gem['Metabolite_Set'].apply(lambda x: x.intersection(valid_mets))
 
     react_gem['Measured_Substrate_Count'] = react_gem['Measured_Substrate'].apply(lambda x: len(x))
     react_gem['Measured_Product_Count'] = react_gem['Measured_Product'].apply(lambda x: len(x))
     react_gem['Measured_Metabolite_Count'] = react_gem['Measured_Metabolite'].apply(lambda x: len(x))
     #react_gem.head()
-    react_gem.to_csv(out_dir + 'all-reactions.tsv', sep='\t', index=False)
+    react_gem.to_csv(out_dir + '/all-reactions.tsv', sep='\t', index=False)
     return react_gem
 
 def react_set_1(react_gem, out_dir):
-    react_set_1_dir = out_dir + '/React-Set-1'
+    react_set_1_dir = out_dir + '/reaction-set-1'
     Path(react_set_1_dir).mkdir(parents=True, exist_ok=True)
     
     react_gem = react_gem[~react_gem['SUBSYSTEM'].isin(['Transport reactions', 'Exchange/demand reactions'])]
@@ -81,7 +95,7 @@ def react_set_1(react_gem, out_dir):
     print(one_more_mets_in.shape)
     
 def react_set_2(react_gem, out_dir):
-    react_set_2_dir = out_dir + '/React-Set-2'
+    react_set_2_dir = out_dir + '/reaction-set-2'
     Path(react_set_2_dir).mkdir(parents=True, exist_ok=True)
     
     react_gem = react_gem[~react_gem['SUBSYSTEM'].isin(['Transport reactions', 'Exchange/demand reactions'])]
@@ -100,7 +114,7 @@ def reverse_equation(eqn):
     return new_eqn
 
 def react_set_3(react_gem, out_dir):
-    react_set_3_dir = out_dir + '/React-Set-3'
+    react_set_3_dir = out_dir + '/reaction-set-3'
     Path(react_set_3_dir).mkdir(parents=True, exist_ok=True)
     
     react_gem = react_gem[~react_gem['SUBSYSTEM'].isin(['Transport reactions', 'Exchange/demand reactions'])]
@@ -134,7 +148,7 @@ def react_set_3(react_gem, out_dir):
     
 
 def react_set_4(react_gem, out_dir):
-    react_set_4_dir = out_dir + '/React-Set-4'
+    react_set_4_dir = out_dir + '/reaction-set-4'
     Path(react_set_4_dir).mkdir(parents=True, exist_ok=True)
     
     react_gem = react_gem[~react_gem['SUBSYSTEM'].isin(['Transport reactions', 'Exchange/demand reactions'])]
@@ -165,15 +179,8 @@ def react_set_4(react_gem, out_dir):
     react_df = pd.concat([react1_df, react2_df, react3_df], axis=0)
     react_df.to_csv(react_set_4_dir + '/reaction-set-4.tsv', sep='\t', index=False)
     
-def str_to_set(cell):
-    print(cell)
-    cell = ''.join(c for c in cell if c not in "'{}")
-    cell = set(cell.split(', '))
-    print(cell)
-    return cell
-    
 def react_set_5(react_gem, out_dir):
-    react_set_5_dir = out_dir + '/React-Set-5'
+    react_set_5_dir = out_dir + '/reaction-set-5'
     Path(react_set_5_dir).mkdir(parents=True, exist_ok=True)
     
     react_gem = react_gem[~react_gem['SUBSYSTEM'].isin(['Transport reactions', 'Exchange/demand reactions'])]
@@ -181,11 +188,6 @@ def react_set_5(react_gem, out_dir):
     print(react_df.shape)
     react_df.to_csv(react_set_5_dir + '/reaction-set-5-basic.tsv', sep='\t', index=False)
     print(react_df.shape)
-
-    react_df['Substrate_Set'] = react_df['Substrate_Set'].apply(lambda x: str_to_set(x))
-    react_df['Product_Set'] = react_df['Product_Set'].apply(lambda x: str_to_set(x))
-    react_df['Measured_Substrate'] = react_df['Measured_Substrate'].apply(lambda x: str_to_set(x))
-    react_df['Measured_Product'] = react_df['Measured_Product'].apply(lambda x: str_to_set(x))
 
     react1_df = react_df[react_df.Direction == 1]
     react2_df = react_df[react_df.Direction == 2]
@@ -201,7 +203,7 @@ def react_set_5(react_gem, out_dir):
     react_df.to_csv(react_set_5_dir + '/reaction-set-5.tsv', sep='\t', index=False)
     
 def react_set_6(react_gem, out_dir):
-    react_set_6_dir = out_dir + '/React-Set-6'
+    react_set_6_dir = out_dir + '/reaction-set-6'
     Path(react_set_6_dir).mkdir(parents=True, exist_ok=True)
     
     react_gem = react_gem[~react_gem['SUBSYSTEM'].isin(['Transport reactions', 'Exchange/demand reactions'])]
@@ -209,11 +211,6 @@ def react_set_6(react_gem, out_dir):
     print(react_df.shape)
     react_df.to_csv(react_set_6_dir + '/reaction-set-6-basic.tsv', sep='\t', index=False)
     print(react_df.shape)
-
-    react_df['Substrate_Set'] = react_df['Substrate_Set'].apply(lambda x: str_to_set(x))
-    react_df['Product_Set'] = react_df['Product_Set'].apply(lambda x: str_to_set(x))
-    react_df['Measured_Substrate'] = react_df['Measured_Substrate'].apply(lambda x: str_to_set(x))
-    react_df['Measured_Product'] = react_df['Measured_Product'].apply(lambda x: str_to_set(x))
 
     react1_df = react_df[react_df.Direction == 1]
     react2_df = react_df[react_df.Direction == 2]
@@ -228,7 +225,7 @@ def react_set_6(react_gem, out_dir):
     react_df.to_csv(react_set_6_dir + '/reaction-set-6.tsv', sep='\t', index=False)
 
 def react_set_7(react_gem, out_dir):
-    react_set_7_dir = out_dir + '/React-Set-7'
+    react_set_7_dir = out_dir + '/reaction-set-7'
     Path(react_set_7_dir).mkdir(parents=True, exist_ok=True)
     
     react_gem = react_gem[~react_gem['SUBSYSTEM'].isin(['Transport reactions', 'Exchange/demand reactions'])]
@@ -239,7 +236,7 @@ def react_set_7(react_gem, out_dir):
     print(two_more_mets_in.shape)
     
 def react_set_8(react_gem, out_dir):
-    react_set_8_dir = out_dir + '/React-Set-8'
+    react_set_8_dir = out_dir + '/reaction-set-8'
     Path(react_set_8_dir).mkdir(parents=True, exist_ok=True)
     
     react_gem = react_gem[~react_gem['SUBSYSTEM'].isin(['Transport reactions', 'Exchange/demand reactions'])]
@@ -272,7 +269,7 @@ def react_set_8(react_gem, out_dir):
     react_df.to_csv(react_set_8_dir + '/reaction-set-8.tsv', sep='\t', index=False)
     
 def react_set_9(react_gem, out_dir):
-    react_set_9_dir = out_dir + '/React-Set-9'
+    react_set_9_dir = out_dir + '/reaction-set-9'
     Path(react_set_9_dir).mkdir(parents=True, exist_ok=True)
     
     react_gem = react_gem[~react_gem['SUBSYSTEM'].isin(['Transport reactions', 'Exchange/demand reactions'])]
@@ -280,11 +277,6 @@ def react_set_9(react_gem, out_dir):
     print(react_df.shape)
     react_df.to_csv(react_set_9_dir + '/reaction-set-9-basic.tsv', sep='\t', index=False)
     print(react_df.shape)
-
-    react_df['Substrate_Set'] = react_df['Substrate_Set'].apply(lambda x: str_to_set(x))
-    react_df['Product_Set'] = react_df['Product_Set'].apply(lambda x: str_to_set(x))
-    react_df['Measured_Substrate'] = react_df['Measured_Substrate'].apply(lambda x: str_to_set(x))
-    react_df['Measured_Product'] = react_df['Measured_Product'].apply(lambda x: str_to_set(x))
 
     react1_df = react_df[react_df.Direction == 1]
     react2_df = react_df[react_df.Direction == 2]
@@ -301,7 +293,18 @@ def react_set_9(react_gem, out_dir):
     
 def main(args):
     Path(args.out_dir).mkdir(parents=True, exist_ok=True)
-    react_gem = all_reactions(args.met_75_path, args.gem_path, args.out_dir)
+    
+    orig_stdout = sys.stdout
+    log_file = open(args.log_path, 'w')
+    sys.stdout = log_file
+    
+    print('gem_path', args.gem_path)
+    print('valid_met_path', args.valid_met_path)
+    print('log_path', args.log_path)
+    print('out_dir', args.out_dir)
+    
+    react_gem = all_reactions(args.valid_met_path, args.gem_path, args.out_dir)
+    
     react_set_1(react_gem, args.out_dir)
     react_set_2(react_gem, args.out_dir)
     react_set_3(react_gem, args.out_dir)
@@ -311,6 +314,9 @@ def main(args):
     react_set_7(react_gem, args.out_dir)
     react_set_8(react_gem, args.out_dir)
     react_set_9(react_gem, args.out_dir)
+    
+    sys.stdout = orig_stdout
+    log_file.close()
     
 if __name__ == "__main__":
     main(parse_args())

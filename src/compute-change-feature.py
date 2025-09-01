45d0a48f0f997dad4b53ff13e13806a4a3aff7bc
Kowshika Sarker <63386569+kowshikasarker@users.noreply.github.com>
2025-09-01 17:56:28 -0500

    Update compute-change-feature.py

--- src/compute-change-feature.py
+++ src/compute-change-feature.py
@@ -6,18 +6,19 @@ import re
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--react_set_path", type=str,
-                        help="path to reaction set in .tsv format",
+    parser.add_argument("--react_set_path", type=str,
+                        help="path to a reaction set in .tsv format",
                         required=True, default=None)
-    parser.add_argument("-c", "--met_change_path", type=str,
+    parser.add_argument("--met_change_path", type=str,
                         help="path to metabolite concentration change in .tsv format", required=True, default=None)
-    parser.add_argument("-m", "--met_path", type=str,
+    parser.add_argument("--valid_met_path", type=str,
                         help="path to list of human-gem overlapped metabolites in .tsv format", required=True, default=None)
-    parser.add_argument("-o", "--out_dir", type=str,
-                        help="path to output dir",
-                        required=True, default=None)
-    parser.add_argument("-l", "--log_path", type=str,
+    
+    parser.add_argument("--log_path", type=str,
                         help="path to log file",
+                        required=True, default=None)
+    parser.add_argument("--out_dir", type=str,
+                        help="path to output dir",
                         required=True, default=None)
     args = parser.parse_args()
     return args
@@ -40,28 +41,26 @@ def main(args):
     
     print('react_set_path', args.react_set_path)
     print('met_change_path', args.met_change_path)
-    print('met_path', args.met_path)
-    print('out_dir', args.out_dir)
+    print('valid_met_path', args.valid_met_path)
     print('log_path', args.log_path)
-    
+    print('out_dir', args.out_dir)
     
     change_df = pd.read_csv(args.met_change_path, sep='\t')
+    change_df[['sample_id', 'sample_group']] = change_df['key'].str.split('.', expand=True)
     
-    change_df[['person_id', 'diet']] = change_df['key'].str.split('.', expand=True)
-    
-    treatments = set(change_df.diet)
+    treatments = set(change_df.sample_group)
     treatments = [treatment for treatment in treatments if not treatment.startswith('No')]
     print('treatments', treatments)
     
     
     
-    change_df = change_df.drop(columns=['person_id', 'diet']).set_index('key')
+    change_df = change_df.drop(columns=['sample_id', 'sample_group']).set_index('key')
     
     
     change_df = change_df.sort_index().sort_index(axis=1)
 
-    id_df = pd.read_csv(args.met_path, sep='\t')
-    met_to_hmdb = dict(zip(id_df.MET_ID, id_df.HMDB_ID))
+    id_df = pd.read_csv(args.valid_met_path, sep='\t')
+    met_to_id = dict(zip(id_df.MET_ID, id_df.ID))
     
     
 
@@ -69,11 +68,6 @@ def main(args):
 
     react_set_df['Measured_Substrate'] = react_set_df['Measured_Substrate'].apply(lambda x: str_to_set(x))
     react_set_df['Measured_Product'] = react_set_df['Measured_Product'].apply(lambda x: str_to_set(x))
-
-    #change_dir = args.out_dir +
-    #pathlib.Path(change_dir).mkdir(parents=True, exist_ok=True)
-    
-    
 
     for treatment in treatments:
         control = 'No' + treatment
@@ -84,16 +78,16 @@ def main(args):
         rc1_df_dict = {}
         for idx, rxn in react_set_df.iterrows():
             rc1_col = 0
+            print(rxn)
             for s in rxn['Measured_Substrate']:
-                if(not s in met_to_hmdb):
+                if(not s in met_to_id):
                     print('s', s, idx, rxn['Measured_Substrate'], rxn)
-                rc1_col = rc1_col - study_change_df[met_to_hmdb[s]]
+                rc1_col = rc1_col - study_change_df[met_to_id[s]]
             for p in rxn['Measured_Product']:
-                if(not p in met_to_hmdb):
+                if(not p in met_to_id):
                     print('p', p, idx, rxn['Measured_Product'], rxn)
-                rc1_col = rc1_col + study_change_df[met_to_hmdb[p]]
+                rc1_col = rc1_col + study_change_df[met_to_id[p]]
             rc1_df_dict[rxn['RXN_ID']] = rc1_col
-            #rc1_df[rxn['RXN_ID']] = rc1_col
         rc1_df = pd.DataFrame(rc1_df_dict)
         rc1_df.index = study_change_df.index
 

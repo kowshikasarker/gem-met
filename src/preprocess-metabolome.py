d71af7044c587dede7b43bd3c8fff4456a88f691
Kowshika Sarker <63386569+kowshikasarker@users.noreply.github.com>
2025-09-01 17:55:02 -0500

    Update preprocess-metabolome.py

--- src/preprocess-metabolome.py
+++ src/preprocess-metabolome.py
@@ -1,73 +1,57 @@
 import argparse
-import pandas as pd
 import sys
 import pathlib
 import numpy as np
+import pandas as pd
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Preprocessing of basleine and end metabolomics profiles.\nBoth profiles are expected to be .tsv files with two columns \person_id\' and \'diet\' indicating sample identifier and assigned diet.\n Other columns are expected to be concentrations of metabolic compounds.\n\n Only rows and coulmns common in both prifles are kept and columns are also filtered based on missing value proportion.\nIn the output directory, 10 .tsv files are generated.')
+    parser = argparse.ArgumentParser(description='Preprocessing of user-provided baseline (before diet) and end (after diet) metabolomic profiles (expected file format: .tsv).' + \
+        '\n' + 'Two columns sample_id and sample_group are expected to have sample id and sample groups (treatment/control).' + \
+        '\n' + 'Other columns are expected to have metabolite abundances. These columns may have arbitrary names.' + \
+        '\n' + 'Rows or columns absent in any profile are discarded from the other profile.' + \
+        '\n' + 'Columns are discarded based on missing value proportion too.' + \
+        '\n' + 'In the output directory, 10 .tsv files are generated.')
     
-    parser.add_argument("-b", "--base_path", type=str,
-                        help="path to baseline metabolomics profile in tsv format",
+    parser.add_argument("--base_path", type=str,
+                        help="path to baseline metabolomics in tsv format",
                         required=True, default=None)
-    parser.add_argument("-e", "--end_path", type=str,
-                        help="path to end metabolomics profile in tsv format",
+    parser.add_argument("--end_path", type=str,
+                        help="path to end metabolomics in tsv format",
                         required=True, default=None)
-    parser.add_argument("-g", "--gem_met_path", type=str,
-                        help="path to human gem metabolites list in tsv format", required=True, default=None)
-    parser.add_argument("-x", "--human_gem_path", type=str,
-                        help="path to human gem model in .xlsx format", required=True, default=None)
-    parser.add_argument("-i", "--id_path", type=str,
-                        help="path to metabolite identifiers in tsv format", required=True, default=None)
-    parser.add_argument("-l", "--log_path", type=str,
+    
+    parser.add_argument("--missing_pct", type=float,
+                        help="threshold of missing value percentage for dropping columns",
+                        required=True, default=None)
+    
+    parser.add_argument("--user_met_id_path", type=str,
+                        help="path to list of user metabolites (columns in in base_path and end_path) with standard identifiers in tsv format",
+                        required=True, default=None)
+    parser.add_argument("--user_met_name_col", type=str,
+                        help="which column in user_met_id_path contains the metabolite names",
+                        required=True, default=None)
+    parser.add_argument("--user_met_id_col", type=str,
+                        help="which column in user_met_id_path contains the metabolite standard identifiers",
+                        required=True, default=None)
+    
+    parser.add_argument("--gem_path", type=str,
+                        help="path to a gem model in .xlsx format",
+                        required=True, default=None)
+    parser.add_argument("--gem_met_id_path", type=str,
+                        help="path to list of human gem metabolites with standard identifiers list in tsv format",
+                        required=True, default=None)
+    parser.add_argument("--gem_met_id_col", type=str,
+                        help="which column in gem_met_id_path contains the metabolite standard identifiers",
+                        required=True, default=None)
+    
+    parser.add_argument("--log_path", type=str,
                         help="path to log file",
                         required=True, default=None)
-    parser.add_argument("-o", "--out_dir", type=str,
+    parser.add_argument("--out_dir", type=str,
                         help="path to output dir",
                         required=True, default=None)
-    parser.add_argument("-m", "--missing_pct", type=float,
-                        help="threshold of missing value percentage for dropping columns", required=True, default=None)
     
     args = parser.parse_args()
     return args
-
-
-# Keep features that have proportion of missing values <  p for any food
-def drop_missing_values(base_df, end_df, missing_pct):
-    print('drop_missing_values')
-    diets = set(base_df['diet']).union(set(end_df['diet']))
-    diets = [diet for diet in diets if not diet.startswith('No')]
-    print(len(diets), ' diets', diets)
-    
-    columns_to_keep_baseline = set()
-    columns_to_keep_end = set()
-
-    for diet in diets:
-        # Select dataset for this study
-        base_df_diet = base_df[base_df['diet'].str.contains(diet)]
-        end_df_diet = end_df[end_df['diet'].str.contains(diet)]
-        
-        # Compute percent of missing values for the datasets
-        missing_pct_base = base_df_diet.isnull().sum() / len(base_df_diet)
-        missing_pct_end = end_df_diet.isnull().sum() / len(end_df_diet)
-
-        # Keep all features that have < p percent missing
-        # i.e. have > p percent features present
-        missing_pct_base = missing_pct_base < missing_pct
-        missing_pct_end = missing_pct_end < missing_pct
-
-        # Subset feature list to only include those features
-        missing_pct_base = missing_pct_base.where(lambda a: a).dropna().index
-        missing_pct_end = missing_pct_end.where(lambda a: a).dropna().index
-        
-        columns_to_keep_baseline.update(list(missing_pct_base))
-        columns_to_keep_end.update(list(missing_pct_end))
-                                   
-    # Subset columns
-    base_df = base_df[list(columns_to_keep_baseline)]
-    end_df = end_df[list(columns_to_keep_end)]
-   
-    return base_df, end_df
 
 # Imputes missing values to uniform random values between [0, mm * minimum observed] for every feature
 def impute_missing_values(base_df, end_df, coeff):
@@ -77,15 +61,18 @@ def impute_missing_values(base_df, end_df, coeff):
     base_df_nan_dict = {}
     end_df_feature_mins = np.min(end_df, axis=0)
     end_df_nan_dict = {}
+    
+    print('base_df_feature_mins', base_df_feature_mins)
+    print('end_df_feature_mins', end_df_feature_mins)
 
     # Create new datasets that contains random values for each subject for each feature,
     # between 0 and mm * the minimum for that feature
-    for feature, minimum in base_df_feature_mins.iteritems():
+    for feature, minimum in base_df_feature_mins.items():
         base_df_nan_dict[feature] = np.random.uniform(
             low=0, high=coeff*minimum, size=len(base_df)
         )
 
-    for feature, minimum in end_df_feature_mins.iteritems():
+    for feature, minimum in end_df_feature_mins.items():
         end_df_nan_dict[feature] = np.random.uniform(
             low=0, high=coeff*minimum, size=len(end_df)
         )
@@ -103,6 +90,7 @@ def impute_missing_values(base_df, end_df, coeff):
 
     return base_df, end_df
 
+
 def main(args):
     pathlib.Path(args.out_dir).mkdir(parents=True, exist_ok=True)
     
@@ -112,7 +100,13 @@ def main(args):
     
     print('base_path', args.base_path)
     print('end_path', args.end_path)
-    print('id_path', args.id_path)
+    print('missing_pct', args.missing_pct)
+    print('user_met_id_path', args.user_met_id_path)
+    print('user_met_name_col', args.user_met_name_col)
+    print('user_met_id_col', args.user_met_id_col)
+    print('gem_path', args.gem_path)
+    print('gem_met_id_path', args.gem_met_id_path)
+    print('gem_met_id_col', args.gem_met_id_col)
     print('log_path', args.log_path)
     print('out_dir', args.out_dir)
     
@@ -124,8 +118,8 @@ def main(args):
     base_df.columns = map(str.lower, base_df.columns)
     end_df.columns = map(str.lower, end_df.columns)
     
-    base_df['key'] = base_df['person_id'] + '.' + base_df['diet']
-    end_df['key'] = end_df['person_id'] + '.' + end_df['diet']
+    base_df['key'] = base_df['sample_id'].astype(str) + '.' + base_df['sample_group'].astype(str)
+    end_df['key'] = end_df['sample_id'].astype(str) + '.' + end_df['sample_group'].astype(str)
     
     base_df = base_df.set_index('key')
     end_df = end_df.set_index('key')
@@ -142,14 +136,25 @@ def main(args):
     base_df = base_df[common_cols]
     end_df = end_df[common_cols]
     
-    print(base_df.shape)
-    print(end_df.shape)
+    print('base_df', base_df.shape, 'end_df', end_df.shape)
     
-    base_df, end_df = drop_missing_values(base_df, end_df, args.missing_pct)
+    base_missing = base_df.isnull().mean()
+    end_missing = end_df.isnull().mean()
     
-    base_df = base_df.drop(['person_id', 'diet'], axis=1)
-    end_df = end_df.drop(['person_id', 'diet'], axis=1)
-   
+    print('base_missing', base_missing)
+    print('end_missing', end_missing)
+    
+    base_drop_cols = base_missing[base_missing >= args.missing_pct].index
+    end_drop_cols = end_missing[end_missing >= args.missing_pct].index
+    
+    print('base_drop_cols', len(base_drop_cols), base_drop_cols)
+    print('end_drop_cols', len(end_drop_cols), end_drop_cols)
+    
+    drop_cols = list(set(base_drop_cols).union(set(end_drop_cols))) + ['sample_id', 'sample_group']
+    print('drop_cols', len(drop_cols), drop_cols)
+    
+    base_df = base_df.drop(columns=drop_cols)
+    end_df = end_df.drop(columns=drop_cols)
     
     print('base_df', base_df.shape, 'end_df', end_df.shape)
     
@@ -170,36 +175,35 @@ def main(args):
     end_df = end_df.sort_index(axis=0).sort_index(axis=1)
     
     print('base_df',base_df.shape, 'end_df', end_df.shape)
-    print()
     
     change_df = end_df.sub(base_df)
     print('change_df', change_df.shape)
     change_df.to_csv(args.out_dir + '/preprocessed_change_name.tsv', sep='\t')
     
-    
-    id_df = pd.read_csv(args.id_path, sep='\t',
-                   usecols=['Name', 'HMDB_ID'])
-    id_df['Name'] = id_df['Name'].apply(lambda x: x.lower())
-    id_df = id_df[id_df.Name.isin(common_cols)]
-    id_df = id_df.dropna(subset=['HMDB_ID'])
+    id_df = pd.read_csv(args.user_met_id_path, sep='\t', usecols=[args.user_met_name_col, args.user_met_id_col])
+    id_df[args.user_met_name_col] = id_df[args.user_met_name_col].apply(lambda x: x.lower())
+    id_df = id_df[id_df[args.user_met_name_col].isin(common_cols)]
+    id_df = id_df.dropna(subset=[args.user_met_id_col])
     
     print('id_df', id_df.shape)
         
-    name_to_hmdb = dict(zip(id_df.Name, id_df.HMDB_ID))
-    hmdb_to_name = dict(zip(id_df.HMDB_ID, id_df.Name))
+    name_to_id = dict(zip(id_df[args.user_met_name_col], id_df[args.user_met_id_col]))
+    print('name_to_id', name_to_id)
     
-    common_cols_hmdb = set([name_to_hmdb[col] for col in common_cols if col in name_to_hmdb.keys()])
-    print(len(common_cols_hmdb))
+    id_to_name = dict(zip(id_df[args.user_met_id_col], id_df[args.user_met_name_col]))
+    print('id_to_name', id_to_name)
     
-    #gem_met = pd.read_csv('/home/ksarker2/Nutrition/New-Metabolomic-Data-Analysis-Phase-2/Human-GEM/model/metabolites.tsv', sep='\t')
-    gem_met = pd.read_csv(args.gem_met_path, sep='\t')
-    gem_met = gem_met.dropna(subset=['metHMDBID'])
+    common_cols_id = set([name_to_id[col] for col in common_cols if col in name_to_id.keys()])
+    print(len(common_cols_id))
+    
+    gem_met = pd.read_csv(args.gem_met_id_path, sep='\t')
+    gem_met = gem_met.dropna(subset=[args.gem_met_id_col])
     print(gem_met.shape)
     
-    common_mets = common_cols_hmdb.intersection(set(gem_met['metHMDBID']))
+    common_mets = common_cols_id.intersection(set(gem_met[args.gem_met_id_col]))
     print('common_mets', len(common_mets))
     
-    common_mets_names = [hmdb_to_name[met] for met in common_mets]
+    common_mets_names = [id_to_name[met] for met in common_mets]
     print(len(common_mets_names))
     print(common_mets_names)
     
@@ -210,47 +214,49 @@ def main(args):
     base_df.to_csv(args.out_dir + '/gem_overlapped_base_name.tsv', sep='\t')
     end_df.to_csv(args.out_dir + '/gem_overlapped_end_name.tsv', sep='\t')
     
-    base_df = base_df.rename(columns=name_to_hmdb)
-    end_df = end_df.rename(columns=name_to_hmdb)
+    base_df = base_df.rename(columns=name_to_id)
+    end_df = end_df.rename(columns=name_to_id)
     
-    base_df.to_csv(args.out_dir + '/gem_overlapped_base_hmdb.tsv', sep='\t')
-    end_df.to_csv(args.out_dir + '/gem_overlapped_end_hmdb.tsv', sep='\t')
+    base_df.to_csv(args.out_dir + '/gem_overlapped_base_id.tsv', sep='\t')
+    end_df.to_csv(args.out_dir + '/gem_overlapped_end_id.tsv', sep='\t')
      
     base_df = base_df.sort_index(axis=0).sort_index(axis=1)
     end_df = end_df.sort_index(axis=0).sort_index(axis=1)
     
     change_df = end_df.sub(base_df)
     
-    change_df.to_csv(args.out_dir + '/gem_overlapped_change_hmdb.tsv', sep='\t')
-    change_df = change_df.rename(columns=hmdb_to_name)
+    change_df.to_csv(args.out_dir + '/gem_overlapped_change_id.tsv', sep='\t')
+    change_df = change_df.rename(columns=id_to_name)
     change_df.to_csv(args.out_dir + '/gem_overlapped_change_name.tsv', sep='\t')
     print('change_df', change_df.shape)
     
     gem_overlapeed_met_names = list(change_df.columns)
-    #print('gem_overlapeed_met_names', gem_overlapeed_met_names)
-    gem_met = gem_met[gem_met['metHMDBID'].isin(hmdb_to_name.keys())]
-    hmdb_to_mam = dict(zip(gem_met.metHMDBID, gem_met.metsNoComp))
-    #print(len(hmdb_to_mam))
-    #print(hmdb_to_mam)
+    gem_met = gem_met[gem_met[args.gem_met_id_col].isin(id_to_name.keys())]
+    id_to_mam = dict(zip(gem_met[args.gem_met_id_col], gem_met.metsNoComp))
+    print('id_to_mam', id_to_mam)
     
-    gem_model = pd.read_excel(args.human_gem_path, sheet_name='METS', usecols=['NAME', 'REPLACEMENT ID'])
-    print('human_gem_path', args.human_gem_path)
-    #gem_model = pd.read_excel('/home/ksarker2/Nutrition/New-Metabolomic-Data-Analysis-Phase-2/Human-GEM/model/Human-GEM.xlsx', sheet_name='METS', usecols=['NAME', 'REPLACEMENT ID'])
-    pattern = '|'.join(['e', 'x', 'm', 'c', 'l', 'r', 'g', 'n', 'i'])
-    gem_model['MAM_ID'] = gem_model['REPLACEMENT ID'].str.replace(pattern, '')
-    gem_model = gem_model[gem_model['MAM_ID'].isin(hmdb_to_mam.values())]
-    #print('gem_model', gem_model.shape)
+    gem_model = pd.read_excel(args.gem_path, sheet_name='METS', usecols=['NAME', 'REPLACEMENT ID'])
+    print('gem_path', args.gem_path)
+    #pattern = '|'.join(['e', 'x', 'm', 'c', 'l', 'r', 'g', 'n', 'i'])
+    #gem_model['MAM_ID'] = gem_model['REPLACEMENT ID'].str.replace(pattern, '')
+    gem_model['MAM_ID'] = gem_model['REPLACEMENT ID'].str[:-1]
+
+    print('gem_model', gem_model.shape)
+    print(list(id_to_mam.values()))
+    print(gem_model['MAM_ID'])
+    gem_model = gem_model[gem_model['MAM_ID'].isin(id_to_mam.values())]
+    print('gem_model', gem_model.shape)
     mam_to_met = dict(zip(gem_model['MAM_ID'], gem_model['NAME']))
-    #print(len(mam_to_met))
-    #print(mam_to_met)
+    print('mam_to_met', mam_to_met)
+    
     
     file = open(args.out_dir + '/gem_overlapped_metabolites.tsv', 'w')
-    file.write('Name' + '\t' + 'HMDB_ID' + '\t' + 'MAM_ID' + '\t' + 'MET_ID' + '\n')
+    file.write('Name' + '\t' + 'ID' + '\t' + 'MAM_ID' + '\t' + 'MET_ID' + '\n')
     for name in gem_overlapeed_met_names:
-        hmdb = name_to_hmdb[name]
-        mam = hmdb_to_mam[hmdb]
+        id = name_to_id[name]
+        mam = id_to_mam[id]
         met = mam_to_met[mam]
-        file.write(name + '\t' + hmdb + '\t' + mam + '\t' + met + '\n')
+        file.write(name + '\t' + id + '\t' + mam + '\t' + met + '\n')
     file.flush()
     file.close()
     

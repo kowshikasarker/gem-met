9b9b6901b39045f23a43241fd1840a63f164e928
Kowshika Sarker <63386569+kowshikasarker@users.noreply.github.com>
2025-10-30 16:53:06 -0500

    Update compute-change-feature.py

--- src/compute-change-feature.py
+++ src/compute-change-feature.py
@@ -45,18 +45,8 @@ def main(args):
     print('log_path', args.log_path)
     print('out_dir', args.out_dir)
     
-    change_df = pd.read_csv(args.met_change_path, sep='\t')
-    change_df[['sample_id', 'sample_group']] = change_df['key'].str.split('.', expand=True)
-    
-    treatments = set(change_df.sample_group)
-    treatments = [treatment for treatment in treatments if not treatment.startswith('No')]
-    print('treatments', treatments)
-    
-    
-    
-    change_df = change_df.drop(columns=['sample_id', 'sample_group']).set_index('key')
-    
-    
+    change_df = pd.read_csv(args.met_change_path, sep='\t', index_col='key')
+       
     change_df = change_df.sort_index().sort_index(axis=1)
 
     id_df = pd.read_csv(args.valid_met_path, sep='\t')
@@ -69,30 +59,36 @@ def main(args):
     react_set_df['Measured_Substrate'] = react_set_df['Measured_Substrate'].apply(lambda x: str_to_set(x))
     react_set_df['Measured_Product'] = react_set_df['Measured_Product'].apply(lambda x: str_to_set(x))
 
-    for treatment in treatments:
-        control = 'No' + treatment
-        study_change_df = change_df[change_df.index.str.contains(treatment)]
-        print('study_change_df', study_change_df.shape)
-        # rc1
-        #rc1_df = study_change_df.copy().drop(columns=study_change_df.columns)
-        rc1_df_dict = {}
-        for idx, rxn in react_set_df.iterrows():
-            rc1_col = 0
-            print(rxn)
-            for s in rxn['Measured_Substrate']:
-                if(not s in met_to_id):
-                    print('s', s, idx, rxn['Measured_Substrate'], rxn)
-                rc1_col = rc1_col - study_change_df[met_to_id[s]]
-            for p in rxn['Measured_Product']:
-                if(not p in met_to_id):
-                    print('p', p, idx, rxn['Measured_Product'], rxn)
-                rc1_col = rc1_col + study_change_df[met_to_id[p]]
-            rc1_df_dict[rxn['RXN_ID']] = rc1_col
-        rc1_df = pd.DataFrame(rc1_df_dict)
-        rc1_df.index = study_change_df.index
-
-        rc1_path = args.out_dir + '/' + treatment + '.' + control + '.change.tsv'
-        rc1_df.to_csv(rc1_path, sep='\t', index=True)
+    rc1_df_dict = {}
+    for idx, rxn in react_set_df.iterrows():
+        rc1_col = 0
+        print(rxn)
+        for s in rxn['Measured_Substrate']:
+            if(not s in met_to_id):
+                print('s', s, idx, rxn['Measured_Substrate'], rxn)
+            rc1_col = rc1_col - change_df[met_to_id[s]]
+        for p in rxn['Measured_Product']:
+            if(not p in met_to_id):
+                print('p', p, idx, rxn['Measured_Product'], rxn)
+            rc1_col = rc1_col + change_df[met_to_id[p]]
+        rc1_df_dict[rxn['RXN_ID']] = rc1_col
+    rc1_df = pd.DataFrame(rc1_df_dict)
+    rc1_df.index = change_df.index
+    
+    rc1_df = rc1_df.iloc[:, :2]
+    
+    df1 = rc1_df.reset_index(names='index')
+    df1[['sample_id', 'sample_group']] = df1['index'].str.split(":", expand=True)
+    df1 = df1.set_index(['sample_id', 'sample_group'])
+    df1 = df1.drop(columns='index')
+    df1.to_csv(args.out_dir + '/reaction.change.tsv', sep='\t', index=True)
+    
+    df2 = pd.concat([change_df, rc1_df], axis=1)
+    df2 = df2.reset_index(names='index')
+    df2[['sample_id', 'sample_group']] = df2['index'].str.split(":", expand=True)
+    df2 = df2.set_index(['sample_id', 'sample_group'])
+    df2 = df2.drop(columns='index')
+    df2.to_csv(args.out_dir + '/metabolite.reaction.change.tsv', sep='\t', index=True)
         
     sys.stdout = orig_stdout
     log_file.close()
